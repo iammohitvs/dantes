164a2c33ff699c4f6f898611d8f7549cdc0e5079
Mohit V S <vsmohit94@gmail.com>
2026-07-27 18:43:46 +0530

    adds tailwind, sadcn components, react router

--- apps/dashboard/src/layouts/RootLayout.tsx
+++ apps/dashboard/src/layouts/RootLayout.tsx
@@ -3,9 +3,9 @@ import { Outlet } from "react-router";
 
 const RootLayout = () => {
   return (
-    <div className="bg-black text-white">
+    <section id="home" className="">
       <Outlet />
-    </div>
+    </section>
   );
 };
 
--- apps/dashboard/src/root.tsx
+++ apps/dashboard/src/root.tsx
@@ -2,6 +2,7 @@ import ReactDom from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router";
 import App from "./App";
 import { StrictMode } from "react";
+import "./globals.css"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import RootLayout from "./layouts/RootLayout";
@@ -9,7 +10,7 @@ import RootLayout from "./layouts/RootLayout";
 const root = document.getElementById("root");
 const queryClient = new QueryClient();
 
-ReactDom.createRoot(root).render(
+ReactDom.createRoot(root as ReactDom.Container).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
