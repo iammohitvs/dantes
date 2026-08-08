7e5dbeb8e8ee1c7b9f04877530304ef6220f2c2b
Mohit V S <vsmohit94@gmail.com>
2026-08-08 23:25:11 +0530

    fixes nginx routing

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
-export const NODE_ENV: string = "production"
+export const NODE_ENV: string = "development"
 
 // https://vite.dev/config/
 export default defineConfig({
