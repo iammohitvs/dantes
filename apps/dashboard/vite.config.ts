e70681d9dcd9039823fe172a6607acdcd7361c07
Mohit V S <vsmohit94@gmail.com>
2026-08-08 18:29:25 +0530

    fixes vite assets base url

--- apps/dashboard/vite.config.ts
+++ apps/dashboard/vite.config.ts
@@ -3,8 +3,11 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
+const NODE_ENV: string = "production";
+
 // https://vite.dev/config/
 export default defineConfig({
+  base: NODE_ENV === "production" ? "/dashboard/" : "",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
